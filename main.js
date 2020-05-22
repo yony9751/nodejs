8726b22752fc2fa53d5bf0bff56baf8c8d46279a
leehayaon <leehayaon@gmail.com>
2020-05-22 14:06:00 +0900

    node13 파일을 이용에 본문 구현

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ var url = require('url'); // node js 의 모듈
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query; // ?id=HTML 입력시 { id = 'HTML'} 로 객체로 호출됨 , queryData.id => HTML 이 추출됨
-    console.log(queryData);
+    var title = queryData.id
     if(_url == '/'){
-      _url = '/index.html';
+      //_url = '/index.html';
+      title = 'welcome!';
     }
     if(_url == '/favicon.ico'){
         response.writeHead(404);
@@ -15,6 +16,29 @@ var app = http.createServer(function(request,response){
 		return;
     }
     response.writeHead(200);
-    response.end(queryData.id); 
+    fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+        var template = `
+        <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <h2>${title}</h2>
+              <p>
+                ${description}
+              </p>
+            </body>
+            </html>
+            `;
+        response.end(template); //함수 안 쪽에 넣어야함
+    });
 });
 app.listen(3000); // 3000번 포트의 node js 를 호출, 포트번호를 생략하면 80임
